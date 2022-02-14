12f6aaddf4f6965272252e125cfe467dba4ac59a
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-02-14 22:03:39 +0300

    Метод для чтения целых чисел из файла

---
+++ Algoritms-CSharp/In.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritms_CSharp
+{
+    public class In
+    {
+        /// <summary>
+        /// Метод для чтения целых чисел из файла
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static IEnumerable<int> ReadInts(string filePath)
+        {
+            using(TextReader reader = File.OpenText(filePath))
+            {
+                string lastLine;
+                while((lastLine = reader.ReadLine()) != null)
+                {
+                    yield return int.Parse(lastLine);
+                }
+            }
+        }
+    }
+}
