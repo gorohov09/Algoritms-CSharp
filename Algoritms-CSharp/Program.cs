27b05e4e4c3661b6c49171ed4d77e503e17f7bbb
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-02-14 22:16:05 +0300

    Проверка работы алгоритма

--- Algoritms-CSharp/Program.cs
+++ Algoritms-CSharp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Algorithms_DataStruct_Lib;
+using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Algoritms_CSharp
 {
@@ -6,7 +9,17 @@ namespace Algoritms_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ints = In.ReadInts(@"D:\Программирование\Data\2Kints.txt").ToArray();
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var triplets = ThreeSum.Count(ints);
+
+            watch.Stop();
+
+            Console.WriteLine($"The number triplets: {triplets}");
+            Console.WriteLine($"Time taken: {watch.ElapsedMilliseconds}");
         }
     }
 }
