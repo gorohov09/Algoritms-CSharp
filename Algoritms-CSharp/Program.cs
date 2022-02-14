e5d8ff77bf1e6e5dfb0f7e8bdd20cbe44c9624c4
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-02-14 21:58:58 +0300

    Добавьте файлы проекта.

---
+++ Algoritms-CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algoritms_CSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
