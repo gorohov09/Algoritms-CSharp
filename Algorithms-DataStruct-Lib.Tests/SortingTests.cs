16a325eed797071043643146a4041709644e9a03
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-02-21 00:18:49 +0300

    Алгоритм быстрой сортировки и его тесты

---
+++ Algorithms-DataStruct-Lib.Tests/SortingTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib.Tests
+{
+    [TestFixture]
+    public class SortingTests
+    {
+        /// <summary>
+        /// Метод, возвращающий базовый массив
+        /// </summary>
+        /// <returns></returns>
+        private int[][] Samples()
+        {
+            int[][] samples = new int[9][];
+            samples[0] = new[] { 1 };
+            samples[1] = new[] { 2, 1 };
+            samples[2] = new[] { 2, 1, 3 };
+            samples[3] = new[] { 1, 1, 1 };
+            samples[4] = new[] { 2, -1, 3, 3 };
+            samples[5] = new[] { 4, -5, 3, 1 };
+            samples[6] = new[] { 1, 3, 12, 1 };
+            samples[7] = new[] { 0, 5, -3, 0 };
+            samples[8] = new[] { 3, 2, 4, 5, 1, 2, 3 };
+
+            return samples;
+        }
+
+        /// <summary>
+        /// Метод, помогающий запустить тест
+        /// </summary>
+        /// <param name="sort"></param>
+        private void RunTestsForSortAlgoritm(Action<int[]> sort)
+        {
+            foreach (var sample in Samples())
+            {
+                sort(sample);
+                CollectionAssert.IsOrdered(sample);
+                PrintOut(sample);
+            }
+        }
+
+        /// <summary>
+        /// Вывод трассировочной информации
+        /// </summary>
+        /// <param name="array"></param>
+        private void PrintOut(int[] array)
+        {
+            TestContext.WriteLine("--------TRACE------------\r");
+            foreach (var el in array)
+            {
+                TestContext.Write(el + " ");
+            }
+            TestContext.WriteLine();
+        }
+
+        [Test]
+        public void BubbleSort_ValidInput_SortedOutput()
+        {
+            Sorting sort = new Sorting();
+            RunTestsForSortAlgoritm(sort.BubbleSort);
+        }
+    }
+}
---
+++ Algorithms-DataStruct-Lib/Sorting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib
+{
+    public class Sorting
+    {
+        /// <summary>
+        /// Алгоритм пузырьковой сортировки
+        /// </summary>
+        /// <param name="array"></param>
+        public void BubbleSort(int[] array)
+        {
+            for (int partIndex = array.Length - 1; partIndex > 0; partIndex--)
+            {
+                for (int i = 0; i < partIndex; i++)
+                {
+                    if (array[i] > array[i + 1])
+                        Swap(array, i, i + 1);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вспомогательный метод, который нужен для перестановки элементов
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        private void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+                return;
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
