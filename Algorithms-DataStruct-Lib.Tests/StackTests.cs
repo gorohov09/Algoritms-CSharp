33351a161195582bb3e1f53e9007bd10490b4a59
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-03-11 13:41:42 +0300

    Стек на основе массива

---
+++ Algorithms-DataStruct-Lib.Tests/StackTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib.Tests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            var stack = new ArrayStack<int>();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Count_PushOneItem()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(12);
+
+            Assert.AreEqual(1, stack.Count);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Peek_Push_Pop_Item()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(12);
+            stack.Push(14);
+            stack.Push(2);
+
+            Assert.AreEqual(2, stack.Peek());
+
+            stack.Pop();
+
+            Assert.AreEqual(14, stack.Peek());
+
+            stack.Pop();
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty);
+
+        }
+    }
+}
