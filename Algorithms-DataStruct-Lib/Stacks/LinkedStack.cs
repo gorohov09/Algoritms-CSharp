2dde96429495a5ca221a9f6762d78b6bd41e98bc
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-03-11 20:34:57 +0300

    Переименовал папку

---
+++ Algorithms-DataStruct-Lib/Stacks/LinkedStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib.Stack
+{
+    public class LinkedStack<T> : IEnumerable<T>
+    {
+        private readonly SinglyLinkedList<T> _list = new SinglyLinkedList<T>();
+
+        public void Push(T item)
+        {
+            _list.AddFirst(item);
+            Count++;
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пустой");
+
+            _list.RemoveFirst();
+            Count--;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пустой");
+
+            return _list.Head.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool IsEmpty { get { return Count == 0; } }
+
+        public int Count { get; private set; }
+    }
+}
