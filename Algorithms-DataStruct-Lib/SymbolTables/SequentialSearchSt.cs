29fe581721d5db7a0608523f87c93d26936cc049
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-03-15 12:55:43 +0300

    Символьная таблица, основанная на тривиальном(последовательном поиске)

---
+++ Algorithms-DataStruct-Lib/SymbolTables/SequentialSearchSt.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib.SymbolTables
+{
+    public class SequentialSearchSt<TKey, TValue>
+    {
+        private class Node
+        {
+            public TKey Key { get; }
+
+            public TValue Value { get; set; }
+
+            public Node Next { get; set; }
+
+            public Node(TKey key, TValue value, Node next)
+            {
+                Key = key;
+                Value = value;
+                Next = next;
+            }
+        }
+
+        private Node _first;
+
+        private readonly EqualityComparer<TKey> _comparer;
+
+        public int Count { get; private set; }
+
+        public bool IsEmpty
+        {
+            get { return Count == 0; }
+        }
+
+        public SequentialSearchSt()
+        {
+            _comparer = EqualityComparer<TKey>.Default;
+        }
+
+        public SequentialSearchSt(EqualityComparer<TKey> comparer)
+        {
+            _comparer = comparer ?? throw new ArgumentNullException();
+        }
+
+        public bool TryGet(TKey key, out TValue val)
+        {
+            for (Node x = _first; x != null; x = x.Next)
+            {
+                if (_comparer.Equals(x.Key, key))
+                {
+                    val = x.Value;
+                    return true;
+                }
+            }
+
+            val = default(TValue);
+            return false;
+        }
+
+        public void Add(TKey key, TValue val)
+        {
+            if (key is null)
+                throw new ArgumentNullException("Key can't be null");
+
+            for (Node x = _first; x != null; x = x.Next)
+            {
+                if (_comparer.Equals(x.Key, key))
+                {
+                    x.Value = val;
+                    return;
+                }
+            }
+
+            _first = new Node(key, val, _first);
+
+            Count++;
+        }
+
+        public bool Contains(TKey key)
+        {
+            for (Node x = _first; x != null; x = x.Next)
+            {
+                if (_comparer.Equals(x.Key, key))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IEnumerable<TKey> Keys()
+        {
+            for (Node x = _first; x != null; x = x.Next)
+            {
+                yield return x.Key;
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (key is null)
+                throw new ArgumentNullException("Key can't be null");
+
+            if ((Count == 1) && (_comparer.Equals(key, _first.Key)))
+            {
+                _first = null;
+                Count = 0;
+                return true;
+            }
+            else
+            {
+                Node prev = null; //Предыдущий узел
+                for (Node x = _first; x != null; x = x.Next)
+                {
+                    if (_comparer.Equals(x.Key, key))
+                    {
+                        if (x == _first)
+                        {
+                            _first = x.Next;
+                        }
+                        else
+                        {
+                            prev.Next = x.Next;
+                        }
+                        Count--;
+                        return true;
+                    }
+                    prev = x;
+                }
+
+                return false;
+            }
+
+        }
+    }
+}
--- Algoritms-CSharp/Program.cs
+++ Algoritms-CSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Algorithms_DataStruct_Lib;
 using Algorithms_DataStruct_Lib.Queues;
 using Algorithms_DataStruct_Lib.Stack;
+using Algorithms_DataStruct_Lib.SymbolTables;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -33,64 +34,12 @@ namespace Algoritms_CSharp
     {
         static void Main(string[] args)
         {
-            var customersList = new List<Customer>()
-            {
-                new Customer { Name = "Ann", Age = 3},
-                new Customer { Name = "Bill", Age = 16},
-                new Customer { Name = "Rose", Age = 20},
-                new Customer { Name = "Rob", Age = 14},
-                new Customer { Name = "Bill", Age = 28},
-                new Customer { Name = "John", Age = 14},
-            };
+            SequentialSearchSt<int, int> dict = new SequentialSearchSt<int, int>();
 
-            var intList = new List<int>() { 1, 4, 2, 7, 5, 9, 12, 3, 4, 12, 3 };
-
-            bool contains = intList.Contains(1);
-            bool contains2 = customersList.Contains(new Customer { Name = "Bill", Age = 28 },
-                new CustomersComparer());
-
-            bool contains3 = customersList.Exists(customer => customer.Name == "Rose");
-
-            var customer = customersList.Max(customer => customer.Age);
-
-            var customers = customersList.Where(customer => customer.Age > 18);
-
-            var list = customersList.FindAll(customer => customer.Name == "Bill");
-
-            var index = customersList.FindIndex(customer => customer.Age == 14);
-
-            
-
-
-
-            foreach (var item in customers)
-            {
-                Console.WriteLine(item.Name);
-            }
-
-            //Console.WriteLine(contains);
-            //Console.WriteLine(contains2);
-        }
-
-        private static bool Exists(int[] array, int number)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] == number)
-                    return true;
-            }
-            return false;
-        }
-
-        private static int IndexOf(int[] array, int number)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] == number)
-                    return i;
-            }
-
-            return -1;
+            dict.Add(1, 1);
+            dict.Add(2, 3);
+            dict.Add(3, 18);
+            dict.Remove(3);
         }
     }
 }
