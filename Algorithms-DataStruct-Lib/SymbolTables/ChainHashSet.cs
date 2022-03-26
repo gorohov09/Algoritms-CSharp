0262f4e2f8d24f9c0dd9005915f65419e7c326a8
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-03-26 13:21:19 +0300

    Хеш-таблица, основанная на методе раздельный цепочек

---
+++ Algorithms-DataStruct-Lib/SymbolTables/ChainHashSet.cs
@@ -0,0 +1,167 @@
+﻿using Algorithms_DataStruct_Lib.Queues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStruct_Lib.SymbolTables
+{
+    public class ChainHashSet<Tkey, TValue> //Шаблонный класс
+    {
+        private SequentialSearchSt<Tkey, TValue>[] _chains; //Массив цепочек
+
+        private const int DefaultCapacity = 4;
+
+        public int Count { get; private set; } //Колличество элементов
+
+        public int Capacity { get; private set; } //Размер массива цепочек
+
+        public ChainHashSet(int capacity)
+        {
+            Capacity = capacity;
+            _chains = new SequentialSearchSt<Tkey, TValue>[Capacity]; //Инициализация массива
+
+            for (int i = 0; i < Capacity; i++)
+            {
+                _chains[i] = new SequentialSearchSt<Tkey, TValue>(); //Инициализация цепочек
+            }
+        }
+
+        public ChainHashSet()
+        {
+            Capacity = DefaultCapacity;
+            _chains = new SequentialSearchSt<Tkey, TValue>[DefaultCapacity];          
+
+            for (int i = 0; i < Capacity; i++)
+            {
+                _chains[i] = new SequentialSearchSt<Tkey, TValue>();
+            }
+        }
+
+        /// <summary>
+        /// Функция хеширования
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private int Hash(Tkey key)
+        {
+            //Битовая маска, чтобы избежать отрицательных ключей
+            return (key.GetHashCode() & 0x7fffffff) % Capacity;
+        }
+
+        public TValue Get(Tkey key)
+        {
+            if (key is null)
+                throw new ArgumentNullException("Key is not allowed to be null");
+
+            int index = Hash(key); //Вычисляем индекс, по которому лежит ключ
+
+            //Используя индекс, получаем доступ к необходимой цепочке и получаем значение
+            #region Под копотом функции TryGet
+            //for (Node x = _first; x != null; x = x.Next)
+            //{
+            //    if (_comparer.Equals(x.Key, key))
+            //    {
+            //        val = x.Value;
+            //        return true;
+            //    }
+            //}
+            #endregion
+
+            if (_chains[index].TryGet(key, out TValue val))
+            {
+                return val;
+            }
+
+            //Если ключ не найден - выбрасываем исключение
+            throw new ArgumentException("Key was not found");
+        }
+
+        public bool Contains(Tkey key)
+        {
+            if (key is null)
+                throw new ArgumentNullException("Key is not allowed to be null");
+
+            int index = Hash(key);
+
+            return _chains[index].Contains(key);
+        }
+
+        public bool Remove(Tkey key)
+        {
+            if (key is null)
+                throw new ArgumentNullException("Key is not allowed to be null");
+
+            int index = Hash(key);
+
+            if (_chains[index].Contains(key)) //Если в цепочке есть ключ
+            {
+                _chains[index].Remove(key); //Удаляем пару по ключу
+                Count--;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Add(Tkey key, TValue val)
+        {
+            if (key is null)
+                throw new ArgumentNullException("Key is not allowed to be null");
+
+            if (val is null)
+            {
+                Remove(key);
+                return;
+            }
+
+            //Когда средняя длина цепочек достигает 10 узлов и больше, удавиваем в размерах массив цепочек
+            if (Count >= 10 * Capacity)
+                Resize(2 * Capacity);
+
+            int i = Hash(key);
+
+            if (!_chains[i].Contains(key)) //Если в цепочке нет такого ключа
+                Count++; //Инкрементируем Count
+
+            //Если ключ уже есть в цепочке, обновляем значение, а Count трогать не надо
+            _chains[i].Add(key, val); //Добавляем пару ключ-значение по вычесленному индексу
+        }
+
+        private void Resize(int chains)
+        {
+            var temp = new ChainHashSet<Tkey, TValue>(chains); //Создаем больший массив цепочек
+
+            //Копируем данные
+            for (int i = 0; i < Capacity; i++) //Проход по текущей таблицы
+            {
+                foreach (var key in _chains[i].Keys()) //Извлекаем пары(ключ-значение) из цепочек
+                {
+                    if (_chains[i].TryGet(key, out TValue val))
+                        temp.Add(key, val); //Добавляем пары во временный массив, большего размера
+                }
+            }
+
+            Capacity = temp.Capacity;
+            Count = temp.Count;
+            _chains = temp._chains; //Перестанавливаем ссылку
+        }
+
+        public IEnumerable<Tkey> Keys()
+        {
+            var queue = new LinkedQueue<Tkey>(); //Создаем очередь ключей
+
+            for (int i = 0; i < Capacity; i++) //Итерируем по массиву цепочек
+            {
+                foreach (var key in _chains[i].Keys())
+                {
+                    queue.Enqueue(key);
+                }
+            }
+
+            return queue;
+        }
+    }
+}
--- Algoritms-CSharp/Program.cs
+++ Algoritms-CSharp/Program.cs
@@ -10,36 +10,76 @@ using System.Linq;
 
 namespace Algoritms_CSharp
 {
-    public class Customer
+    public class PhoneNumber
+    {
+        public string AreaCode { get; }
+
+        public string Exchange { get; }
+
+        public string Number { get; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as PhoneNumber;
+
+            if (other is null)
+                return false;
+
+            return this.AreaCode == other.AreaCode
+                && this.Exchange == other.Exchange
+                && this.Number == other.Number;
+        }
+
+        public static bool operator ==(PhoneNumber left, PhoneNumber right)
+        {
+            if (object.ReferenceEquals(left, right))
+                return true;
+
+            if (object.ReferenceEquals(null, left))
+                return false;
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(PhoneNumber left, PhoneNumber right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(AreaCode, Exchange, Number);
+        }
+    }
+
+    public class Person
     {
         public string Name { get; set; }
 
         public int Age { get; set; }
-    }
 
-    public class CustomersComparer : IEqualityComparer<Customer>
-    {
-        public bool Equals(Customer x, Customer y)
-        {
-            return x.Age == y.Age && x.Name == y.Name;
-        }
-
-        public int GetHashCode([DisallowNull] Customer obj)
-        {
-            return obj.GetHashCode();
-        }
+        public int Ssh { get; set; }
     }
 
     public class Program
     {
         static void Main(string[] args)
         {
-            SequentialSearchSt<int, int> dict = new SequentialSearchSt<int, int>();
+            ChainHashSet<string, int> table = new ChainHashSet<string, int>(5);
 
-            dict.Add(1, 1);
-            dict.Add(2, 3);
-            dict.Add(3, 18);
-            dict.Remove(3);
+            table.Add("a", 3);
+            table.Add("b", 4);
+            table.Add("c", 5);
+            table.Add("d", 6);
+            table.Add("dfdf", 45);
+            table.Add("4567", 12);
+
+            var keys = table.Keys();
+
+            foreach (string key in keys)
+            {
+                Console.WriteLine(key);
+            }
         }
     }
 }
