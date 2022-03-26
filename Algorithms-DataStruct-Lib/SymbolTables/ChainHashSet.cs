0091f5fa1527fdb10ce9c9d41c57f98487683dc3
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-03-26 13:47:12 +0300

    Изменение методов Add и Remove класса ChainHashSet

--- Algorithms-DataStruct-Lib/SymbolTables/ChainHashSet.cs
+++ Algorithms-DataStruct-Lib/SymbolTables/ChainHashSet.cs
@@ -100,6 +100,10 @@ namespace Algorithms_DataStruct_Lib.SymbolTables
                 _chains[index].Remove(key); //Удаляем пару по ключу
                 Count--;
 
+                //Сужение размерности массива
+                if (Capacity > DefaultCapacity && Count <= 2 * Capacity)
+                    Resize(Prime.ReducePrime(Capacity));
+
                 return true;
             }
 
@@ -119,7 +123,7 @@ namespace Algorithms_DataStruct_Lib.SymbolTables
 
             //Когда средняя длина цепочек достигает 10 узлов и больше, удавиваем в размерах массив цепочек
             if (Count >= 10 * Capacity)
-                Resize(2 * Capacity);
+                Resize(Prime.ExpendPrime(Capacity));
 
             int i = Hash(key);
 
